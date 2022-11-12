12a7b181dec2424363e47922e809d60b10afcca4
diz233 <dizhen@hsph.harvard.edu>
2022-11-11 21:39:40 -0500

    added dual_number methods and passed 9 tests

--- src/fbi/dual_number.py
+++ src/fbi/dual_number.py
@@ -38,7 +38,7 @@ class dual_number:
         if var_type(val):
             self._val = val
         else:
-            raise TypeError('ERROR: Input value should be an int or float')
+            raise TypeError('Error: Input value should be an int or float')
 
     @property
     def derv(self):
@@ -53,11 +53,145 @@ class dual_number:
             try:
                 derv = derv.astype(float)
             except ValueError:
-                raise ValueError('ERROR: Input value should be an int or float')
+                raise ValueError('Error: Input value should be an int or float')
             self._derv = derv
         # for all other non-numeric cases, raise the appropriate value error
         else:
-            raise TypeError('ERROR: Input value must be an array of ints/floats or be a scalar int/float')
+            raise TypeError('Error: Input value must be an array of ints/floats or be a scalar int/float')
 
     def __repr__(self):
-        return f'Values:{self.val}, Derivatives:{self.derv}'
+        return f'Values: {self.val}, Derivatives: {self.derv}'
+    
+    def __add__(self, other):
+        # perform addition if other is a dual number
+        try:
+            f = self.val + other.val
+            f_prime = self.derv + other.derv
+        # perform addition if other is a real number
+        except AttributeError:
+            f = self.val + other
+            f_prime = self.derv
+        return dual_number(f, f_prime)
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __sub__(self, other):
+        # perform subtraction if other is a dual number
+        try:
+            f = self.val - other.val
+            f_prime = self.derv - other.derv
+        # perform subtraction if other is a real number
+        except AttributeError:
+            f = self.val - other
+            f_prime = self.derv
+        return dual_number(f, f_prime)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __mul__(self, other):
+        # perform multiplication if other is a dual number
+        try:
+            f = self.val * other.val
+            f_prime = self.val * other.derv + self.derv * other.val
+        # perform multiplication if other is a real number
+        except AttributeError:
+            f = self.val * other
+            f_prime = self.derv * other
+        return dual_number(f, f_prime)
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        # perform division if other is a dual number
+        try:
+            # avoid zero division
+            if other.val == 0:
+                raise ZeroDivisionError("Error: Denominator in division should not be 0")
+            f = self.val / other.val
+            f_prime = (self.derv * other.val - self.val * other.derv) / (other.val ** 2)
+            return dual_number(f, f_prime)
+        # perform division if other is a real number
+        except AttributeError:
+            # avoid zero division
+            if other == 0:
+                raise ZeroDivisionError("Error: Denominator in division should not be 0")
+            f = self.val / other
+            f_prime = self.derv / other
+            return dual_number(f, f_prime)
+        
+    def __rtruediv__(self, other):
+        if self.val == 0:
+            raise ZeroDivisionError("Error: Denominator in division should not be 0")
+        f = other / self.val
+        f_prime = (- other * self.derv) / (self.val ** 2)
+        return dual_number(f, f_prime)
+
+    def __pow__(self, other):
+        # perform power operation if other is a dual number
+        try:
+            # ensure user does not raise a negative number to a fraction with an even denominator
+            if self.val < 0 and other.val % 1 != 0 and other.val.as_integer_ratio()[1] % 2 == 0:
+                raise ValueError("Error: Attempted to raise a negative number to a fraction with even denominator")
+            # ensure user does not have a 0 derivative when the exponent is less than 1
+            if self.val == 0 and other.val < 1:
+                raise ValueError("Error: Attempted to find derivative at 0 when exponent is less than 1")
+
+            f = self.val ** other.val
+            # compute the derivative power rule for a dual number exponent
+            f_prime = (self.val ** (other.val - 1)) * self.derv * other.val + (
+                    self.val ** other.val) * other.derv * np.log(self.val)
+            return dual_number(f, f_prime)
+
+        # perform power operation if other is a real number
+        except AttributeError:
+            # ensure user does not raise a negative number to a fraction with an even denominator
+            if self.val < 0 and other % 1 != 0 and other.as_integer_ratio()[1] % 2 == 0:
+                raise ValueError("ERROR: Attempted to raise a negative number to a fraction with even denominator")
+            # ensure user does not have a 0 derivative when the exponent is less than 1
+            if self.val == 0 and other < 1:
+                raise ValueError("ERROR: Attempted to find derivative at 0 when exponent is less than 1")
+
+            f = self.val ** other
+            # compute the derivative power rule for a real number exponent
+            f_prime = other * self.val ** (other - 1)
+            return dual_number(f, self.derv * f_prime)
+        
+    def __rpow__(self, other):
+        f = other ** self.val
+        f_prime = (other ** self.val) * self.derv * np.log(other)
+        return dual_number(f, f_prime)
+    
+    def __neg__(self):
+        return dual_number(-1 * self.val, -1 * self.derv)
+    
+    def __eq__(self, other):
+        # check if val_derv values are equal
+        try:
+            value_eq = all(self.val == other.val)
+        except TypeError:
+            value_eq = True if self.val == other.val else False
+
+        # check if val_derv derivatives are equal
+        try:
+            derivative_eq = all(self.derv == other.derv)
+        except TypeError:
+            derivative_eq = True if self.derv == other.derv else False
+        return value_eq, derivative_eq
+    
+    def __ne__(self, other):
+        # check if val_derv values are not equal
+        try:
+            value_eq = all(self.val != other.val)
+        except TypeError:
+            value_eq = True if self.val != other.val else False
+
+        # check if val_derv derivatives are not equal
+        try:
+            derivative_eq = all(self.derv != other.derv)
+        except TypeError:
+            derivative_eq = True if self.derv != other.derv else False
+
+        return value_eq, derivative_eq
--- src/tests/test_dual_number.py
+++ src/tests/test_dual_number.py
@@ -1,18 +1,96 @@
 import pytest
-
-# import names to test
+import numpy as np
 from fbi.dual_number import dual_number, var_type
 
-z1 = dual_number(1,2)
+z0 = dual_number(1, 2)
+z1 = dual_number(1, -1)
+z2 = dual_number(2.2, 0)
+z3 = dual_number(2, 6.036)
+z4 = dual_number(3, 100000)
+z5 = dual_number(0.5, -2)
+
 class TestDualNumber:
     """Test class for dual_number module"""
+    
+    def test_var_type(self):
+        x1, x2, x3, x4 = 10, 'a', 'deriv', ['d', 'e', 'r', 'i', 'v']
+        x5, x6, x7, x8 = [1, 'e', 2, 'i', 'v'], [1, 2, 3, 4, 5], \
+                                   ["test", 3, 0, 'c'], [0.1, 0.2, -10]
 
-    def test_init(self):
-        assert z1.val == 1
-        assert z1.derv == 2
+        assert var_type(x1) == True
+        assert var_type(x2) == False
+        assert var_type(x3) == False
+        assert var_type(x4) == False
+        assert var_type(x5) == False
+        assert var_type(x6) == True
+        assert var_type(x7) == False
+        assert var_type(x8) == True
         
-    def test_setter(self):
-        z1.val = 2
-        z1.derv = 1
-        assert z1.val == 2
-        assert z1.derv == 1
+    def test_init(self):
+        assert z0.val == 1
+        assert z0.derv == 2
+    
+    def test_add(self):
+        out = z1 + z2
+        assert out.val == 3.2
+        assert out.derv == -1
+        out = z1 + 2
+        assert out.val == 3
+        assert out.derv == -1
+        out = 2 + z1
+        assert out.val == 3
+        assert out.derv == -1
+        
+    def test_sub(self):
+        out = z1 - z0
+        assert out.val == 0
+        assert out.derv == -3
+        out = z1 - 2
+        assert out.val == -1
+        assert out.derv == -1
+        out = 2 - z1
+        assert out.val == 1
+        assert out.derv == 1
+        
+    def test_mul(self):
+        out = z1 * z2
+        assert out.val == 2.2
+        assert out.derv == -2.2
+        out = z1 * 2
+        assert out.val == 2
+        assert out.derv == -2
+        out = 2 * z1
+        assert out.val == 2
+        assert out.derv == -2
+        
+    def test_div(self):
+        out = z1 / z3
+        assert out.val == 0.5
+        assert out.derv == -8.036 / 4
+        out = z1 / 2
+        assert out.val == 0.5
+        assert out.derv == -0.5
+        out = 2 / z1
+        assert out.val == 2
+        assert out.derv == 2
+        
+    def test_pow(self):
+        out = z5 ** z4
+        assert out.val == 0.5 ** 3
+        assert out.derv == (0.5 ** 3) * (100000 * np.log(0.5) + (-2) * 3 / 0.5)
+        out = z4 ** 2
+        assert out.val == 3 ** 2
+        assert out.derv == (3 ** 2) * (100000 * 2 / 3)
+        out = 2 ** z4
+        assert out.val == 2 ** 3
+        assert out.derv == (2 ** 3) * (100000 * np.log(2))
+        
+    def test_neg(self):
+        out = -z1
+        assert out.val == -1
+        assert out.derv == 1
+        
+    def test_eq(self):
+        tmp = dual_number(1,-1)
+        assert tmp.val == z1.val
+        assert tmp.derv == z1.derv

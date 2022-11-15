898e242e1d687a482d6fa9a014a8656ffaa36cc6
MengyaoZheng <mengyaozheng@hsph.harvard.edu>
2022-11-15 13:45:59 -0500

    ZMY: commit description

---
+++ src/fbi/forward_mode.py
@@ -0,0 +1,7 @@
+# Authors: Wenxu Liu, Queenie Luo, Guangya Wan, Mengyao Zheng, Di Zhen                 #
+# Course: AC207/CS107                                                                  #
+# File: dual_number.py                                                                 #
+# Description: Perform forward mode automatic differentiation method, enabling a user  #
+# to output just the function values, just the derivative values, or both the function #
+# and derviative values in a tuple
+########################################################################################

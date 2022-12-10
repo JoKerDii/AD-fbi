8623123d0c7d8619b1234ccbda790f9b7bc7379f
MengyaoZheng <mengyaozheng@hsph.harvard.edu>
2022-12-09 20:05:19 -0500

    add test for gradient_descent

--- src/tests/test_optimizers.py
+++ src/tests/test_optimizers.py
@@ -41,3 +41,24 @@ class TestOptimizer:
             Optimizer.momentum(xy, f_xy, 1000, alpha = 10)
         except ValueError:
             assert True
+            
+    # test univariate function for gradient descent optimizer
+    def test_uni_gradient_descent(self):
+        opt_time, val, curr_val = Optimizer.momentum(x, f_x, 10000)
+        assert val == pytest.approx(0)
+        assert curr_val[0] == pytest.approx(-1)
+        
+    # test multivariate function for momentum optimizer
+    def test_multi_gradient_descent(self):
+        opt_time, val, curr_val = Optimizer.gradient_descent(xy, f_xy, 10000)
+        assert val == pytest.approx(0)
+        assert curr_val[0] == pytest.approx(1)
+        assert curr_val[1] == pytest.approx(-1)
+        
+            
+    # test invalid hyper parameters for momentum
+    def test_gradient_descent_alpha_invalid(self):
+        try:
+            Optimizer.gradient_descent(xy, f_xy, 1000, alpha = 10)
+        except ValueError:
+            assert True
